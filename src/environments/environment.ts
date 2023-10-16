fe5122e037829c25e746ea2086e296cb2a32c17c
thi4go-git <thi4go19@gmail.com>
2023-10-16 10:53:44 -0300

    Ajuste URL Backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    //apiUrl: 'http://cloudtecnologia.dynns.com:8090',
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://cloudtecnologia.dynns.com:8090',
+    //apiUrl: 'http://localhost:8080',
     cli_id: 'my-angular-app',
     cli_secret: '@321',
     versao: 'v_16.10.2023'
