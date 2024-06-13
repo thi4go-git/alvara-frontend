9e549ebbfd96ee5354f00dafe81b0d07ae8808b9
Thiago Melo <thi4go19@gmail.com>
2024-06-13 15:13:01 -0300

    ajustes docker

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://cloudtecnologia.dynns.com:8002',
-    //apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://cloudtecnologia.dynns.com:8090',
+    //apiUrl: 'http://127.0.0.1:8090',
     cli_id: 'my-angular-app',
     cli_secret: '@321',
-    versao: 'v_19.03.2024'
+    versao: 'v_13.06.2024'
 };
